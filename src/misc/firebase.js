54746bb2bdc3523bf3af553572b538f31507060a
hansu-17 <69525427+hansu-17@users.noreply.github.com>
2021-06-15 19:52:44 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyAbOqVCHM1oib0GwEIChdJmO1smW5BwgY4',
+  authDomain: 'chat-app-2f602.firebaseapp.com',
+  databaseURL: 'https://chat-app-2f602-default-rtdb.firebaseio.com',
+  projectId: 'chat-app-2f602',
+  storageBucket: 'chat-app-2f602.appspot.com',
+  messagingSenderId: '704400879065',
+  appId: '1:704400879065:web:096e7bb67dce1b77037dc8',
+};
+
+const app = firebase.initializeApp(config);
