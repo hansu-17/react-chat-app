063cc57e2d8ceb173453ba14649ac0142f28bf41
hansu-17 <69525427+hansu-17@users.noreply.github.com>
2021-06-17 16:26:47 +0530

    displayed user data with modal window

---
+++ src/components/chat-window/messages/ProfileInfoBtnModal.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Button, Modal } from 'rsuite';
+import { useModalState } from '../../../misc/custom-hooks';
+import ProfileAvatar from '../../ProfileAvatar';
+
+const ProfileInfoBtnModal = ({ profile, ...btnProps }) => {
+  const { isOpen, open, close } = useModalState();
+
+  const shortName = profile.name.split(' ')[0];
+
+  return (
+    <>
+      <Button {...btnProps} onClick={open}>
+        {shortName}
+      </Button>
+      <Modal show={isOpen} onHide={close}>
+        <Modal.Header>
+          <Modal.Title>{shortName} Profile</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body className="text-center">
+          <ProfileAvatar
+            src={profile.avatar}
+            name={profile.name}
+            className="width-200 height-200 img-fullsize font-huge"
+          />
+
+          <h4 className="mt-2">{profile.name}</h4>
+
+          <p>Member since {new Date(profile.createdAt).toLocaleDateString()}</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button block onClick={close}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default ProfileInfoBtnModal;
