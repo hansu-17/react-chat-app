e805cadab3132a0f0a34f18d23d7e094f3a4d167
hansu-17 <69525427+hansu-17@users.noreply.github.com>
2021-06-18 21:33:59 +0530

    manage fcm from client; call cloud function and send fcm

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,8 +1,11 @@
+import { Notification as Toast } from 'rsuite';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
 import 'firebase/messaging';
+import 'firebase/functions';
+import { isLocalHost } from './helper';
 
 const config = {
   apiKey: 'AIzaSyAbOqVCHM1oib0GwEIChdJmO1smW5BwgY4',
@@ -19,6 +22,7 @@ const app = firebase.initializeApp(config);
 export const auth = app.auth();
 export const database = app.database();
 export const storage = app.storage();
+export const functions = app.functions('europe-west3');
 
 // check messaging is available at browser or not
 export const messaging = firebase.messaging.isSupported
@@ -30,7 +34,13 @@ if (messaging) {
     'BOlqb5ZlTmd2-GMxUBAtaFSGdGt6kzs8_0YDELZWu6NfaPi4UxAdBEEoAkHFFxZobueHVKd4MNaOsW3QqbZO-L4'
   );
 
-  messaging.onMessage(data => {
-    console.log(data);
+  messaging.onMessage(({ notification }) => {
+    const { title, body } = notification;
+
+    Toast.info({ title, description: body, duration: 0 });
   });
+}
+
+if (isLocalHost) {
+  functions.useFunctionsEmulator('http://localhost:5001');
 }
